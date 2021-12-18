8113d947ba2072d374ab2792697ecbdc67da45ad
Kirill12a <kirilldrozd12@gmail.com>
2021-12-18 23:19:07 +0300

    создал collectionView

---
+++ CollectionViewWithoutStoryboard/Profile.swift
@@ -0,0 +1,16 @@
+//
+//  Profile.swift
+//  CollectionViewWithoutStoryboard
+//
+//  Created by Kirill Drozdov on 18.12.2021.
+//
+
+import Foundation
+
+
+struct Profile {
+    let name: String
+    let location: String
+    let imageName: String
+    let profession: String
+}
--- CollectionViewWithoutStoryboard/ViewController.swift
+++ CollectionViewWithoutStoryboard/ViewController.swift
@@ -1,19 +1,120 @@
 //
 //  ViewController.swift
-//  CollectionViewWithoutStoryboard
+//  UICollectionViewDemo
 //
-//  Created by Kirill Drozdov on 17.12.2021.
+//  Created by Jayesh Kawli on 12/21/19.
+//  Copyright © 2019 Jayesh Kawli. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    var profiles: [Profile] = []
+
+    private let collectionView: UICollectionView = {
+        let viewLayout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: viewLayout)
+        collectionView.backgroundColor = .white
+        return collectionView
+    }()
+
+    private enum LayoutConstant {
+        static let spacing: CGFloat = 16.0
+        static let itemHeight: CGFloat = 20 // высота ячеек
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupViews()
+        setupLayouts()
+        populateProfiles()
+        collectionView.reloadData()
+    }
+
+    private func setupViews() {
+        view.backgroundColor = .brown // был белый, для проверки поставил борун
+        view.addSubview(collectionView)
+
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(ProfileCell.self, forCellWithReuseIdentifier: ProfileCell.identifier)
+    }
+
+    private func setupLayouts() {
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+
+        // Layout constraints for `collectionView`
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            collectionView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            collectionView.rightAnchor.constraint(equalTo: view.rightAnchor)
+        ])
+    }
+
+    private func populateProfiles() {
+        profiles = [
+            Profile(name: "Thor", location: "Boston", imageName: "face", profession: "astronomy"),
+            Profile(name: "Mike", location: "Albequerque", imageName: "face", profession: "basketball"),
+            Profile(name: "Walter White", location: "New Mexico", imageName: "face", profession: "chemistry"),
+            Profile(name: "Sam Brothers", location: "California", imageName: "face", profession: "geography"),
+            Profile(name: "Chopin", location: "Norway", imageName: "face", profession: "geometry"),
+            Profile(name: "Castles", location: "UK", imageName: "face", profession: "history"),
+            Profile(name: "Dr. Johnson", location: "Australia", imageName: "face", profession: "microscope"),
+            Profile(name: "Tom Hanks", location: "Bel Air", imageName: "face", profession: "theater"),
+            Profile(name: "Roger Federer", location: "Switzerland", imageName: "face", profession: "trophy"),
+            Profile(name: "Elon Musk", location: "San Francisco", imageName: "face", profession: "graduate")
+        ]
     }
 
 
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return profiles.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProfileCell.identifier, for: indexPath) as! ProfileCell
+
+        let profile = profiles[indexPath.row]
+        cell.setup(with: profile)
+        cell.contentView.backgroundColor = .red
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+        let width = itemWidth(for: view.frame.width, spacing: LayoutConstant.spacing)
+
+        return CGSize(width: width, height: LayoutConstant.itemHeight)
+    }
+
+    func itemWidth(for width: CGFloat, spacing: CGFloat) -> CGFloat {
+        let itemsInRow: CGFloat = 2
+
+        let totalSpacing: CGFloat = 2 * spacing + (itemsInRow - 1) * spacing
+        let finalWidth = (width - totalSpacing) / itemsInRow
+
+        return floor(finalWidth)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: LayoutConstant.spacing, left: LayoutConstant.spacing, bottom: LayoutConstant.spacing, right: LayoutConstant.spacing)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return LayoutConstant.spacing
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return LayoutConstant.spacing
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(profiles[indexPath.row].name)
+    }
+}
